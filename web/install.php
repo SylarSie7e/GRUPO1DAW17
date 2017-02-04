be98192c38f0ae82970dc4f47d3fa6a4d01f35d2
SylarSie7e <fradiazalvarez@gmail.com>
2017-02-04 02:27:51 +0100

    Update install.php

--- web/install.php
+++ web/install.php
@@ -1,27 +1,27 @@
 <?php
-include_once("../app/Config.php");
+include_once("/../app/Config.php");
 
 $host=Config::$mvc_bd_hostname;
 $user=Config::$mvc_bd_usuario;
 $pass=Config::$mvc_bd_clave;
 
 $conexion=mysqli_connect($host,$user,$pass) or die ("Imposible conectar");
-if (mysqli_query($conexion,"CREATE DATABASE IF NOT EXISTS alimentos")) {
+if (mysqli_query($conexion,"CREATE DATABASE IF NOT EXISTS u386099923_bdd")) {
 	echo "Se ha creado la base de datos";
 } else {
 	echo "Ya existia la base de datos<br>";
 }
-mysqli_select_db($conexion,"alimentos");
-$alimentos="CREATE TABLE IF NOT EXISTS `alimentos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(255) NOT NULL,
-  `energia` decimal(10,0) NOT NULL,
-  `proteina` decimal(10,0) NOT NULL,
-  `hidratocarbono` decimal(10,0) NOT NULL,
-  `fibra` decimal(10,0) NOT NULL,
-  `grasatotal` decimal(10,0) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+mysqli_select_db($conexion,"u386099923_bdd");
+$alimentos="CREATE TABLE IF NOT EXISTS alimentos (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  nombre varchar(255) NOT NULL,
+  energia decimal(10,0) NOT NULL,
+  proteina decimal(10,0) NOT NULL,
+  hidratocarbono decimal(10,0) NOT NULL,
+  fibra decimal(10,0) NOT NULL,
+  grasatotal decimal(10,0) NOT NULL,
+  PRIMARY KEY (id)
+)
 ";
 
 
@@ -33,3 +33,4 @@ if (mysqli_query($conexion,$alimentos)) {
 
 
 ?>
+
