6c187b9694a64e09fd9f2de030eb0e6983c6b67f
MMAARR <calamonte15@gmail.com>
2017-02-04 02:45:23 +0100

    Update Config.php

--- app/Config.php
+++ app/Config.php
@@ -3,11 +3,11 @@
  class Config
  {
      static public $mvc_bd_hostname = "localhost";
-     static public $mvc_bd_nombre   = "alimentos";
-     static public $mvc_bd_usuario  = "root";
-     static public $mvc_bd_clave    = "";
+     static public $mvc_bd_nombre   = "u804839221_alime";
+     static public $mvc_bd_usuario  = "u804839221_mar";
+     static public $mvc_bd_clave    = "Cala2016";
      static public $mvc_vis_css     = "estilo.css";
-     static public $mvc_colorBox_css = "colorbox.css";
- 	 static public $mvc_jBox = "jquery.colorbox.js";
+   	 static public $mvc_colorBox_css     = "colorbox.css";
+	 static public $mvc_jBox    = "jquery.colorbox.js";
  }
 ?>
